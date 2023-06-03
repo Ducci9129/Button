152fec125fc2dc3aa7e9dc1b80fcf999ec5ee1ca
Ducci9129 <106346483+Ducci9129@users.noreply.github.com>
2023-06-03 02:29:23 -0400

    Update script.js
    progress now saves

--- script.js
+++ script.js
@@ -2,18 +2,26 @@
 const button = document.getElementById('clickButton');
 const clickCountElement = document.getElementById('clickCount');
 
-// Initialize the click count
-let clickCount = 0;
-let test = 0;
-let upgrade = 50;
-let addScore = 1;
-// Update the click count on button click, accidental infinite Button Event Listener things.
+// Initialize the variables
+let clickCount = parseInt(localStorage.getItem('clickCount')) || 0;
+let upgrade = parseInt(localStorage.getItem('upgrade')) || 50;
+let addScore = parseInt(localStorage.getItem('addScore')) || 1;
+
+// Update the click count on button click
 button.addEventListener('click', function() {
   if (clickCount >= upgrade) {
     upgrade = upgrade * 5;
     addScore = addScore * 4;
-    window.alert("next upgrade at " + upgrade + "\nEarning " + addScore + " per click");
+    window.alert("Next upgrade at " + upgrade + "\nEarning " + addScore + " per click");
   }
   clickCount = clickCount + addScore;
   clickCountElement.textContent = clickCount;
+
+  // Store the updated variables in localStorage
+  localStorage.setItem('clickCount', clickCount);
+  localStorage.setItem('upgrade', upgrade);
+  localStorage.setItem('addScore', addScore);
 });
+
+// Display the stored variables on page load
+clickCountElement.textContent = clickCount;
